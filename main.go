fccd5d20dca04772466ca0df12d9fd0d3f6abe31
Mark Henderson <henderson.mark@gmail.com>
2021-06-09 15:00:20 -0400

    gofmt main.go

--- main.go
+++ main.go
@@ -1,33 +1,28 @@
 package main
 
-// import "log"
-
-// import "github.com/onflow/cadence"
-
 import (
-    "github.com/onflow/flow-go-sdk"
-    "github.com/onflow/flow-go-sdk/crypto"
-    // "github.com/onflow/flow-go-sdk/templates"
+	"github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/crypto"
 )
 
 func ECDSAKeygen(seed []byte) (crypto.PrivateKey, error) {
-    return crypto.GeneratePrivateKey(crypto.ECDSA_P256, seed)
+	return crypto.GeneratePrivateKey(crypto.ECDSA_P256, seed)
 }
 
 func FlowAccountKeygen(privateKey crypto.PrivateKey) *flow.AccountKey {
-    publicKey := privateKey.PublicKey()
+	publicKey := privateKey.PublicKey()
 
-    accountKey := flow.NewAccountKey().
-        SetPublicKey(publicKey).
-        SetHashAlgo(crypto.SHA3_256).               // pair this key with the SHA3_256 hashing algorithm
-        SetWeight(flow.AccountKeyWeightThreshold)   // give this key full signing weight
+	accountKey := flow.NewAccountKey().
+		SetPublicKey(publicKey).
+		SetHashAlgo(crypto.SHA3_256).             // pair this key with the SHA3_256 hashing algorithm
+		SetWeight(flow.AccountKeyWeightThreshold) // give this key full signing weight
 
-   return accountKey
+	return accountKey
 }
 
 func main() {
-    seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
-    pk, _ := ECDSAKeygen(seed) // TODO err handling
+	seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
+	pk, _ := ECDSAKeygen(seed) // TODO err handling
 
-    FlowAccountKeygen(pk)
+	FlowAccountKeygen(pk)
 }
