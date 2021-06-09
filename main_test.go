bf1462e81ae3cf824555be3d36a6ff808a91c08c
Mark Henderson <henderson.mark@gmail.com>
2021-06-09 12:32:35 -0400

    broken account creation script

--- main_test.go
+++ main_test.go
@@ -3,9 +3,12 @@ package main
 import (
     "testing"
     "google.golang.org/grpc"
-    "github.com/stretchr/testify/assert"
+    "github.com/onflow/flow-go-sdk"
     "github.com/onflow/flow-go-sdk/crypto"
     "github.com/onflow/flow-go-sdk/client"
+    "github.com/onflow/flow-go-sdk/examples"
+    "github.com/onflow/flow-go-sdk/templates"
+    "github.com/stretchr/testify/assert"
 )
 
 func TestECDSAKeygen(t *testing.T) {
@@ -23,12 +26,28 @@ func TestFlowAccountFromKeygen(t *testing.T) {
     sk, err := ECDSAKeygen(seed)
     assert.NoError(t, err)
 
-    flowAccount := FlowAccountKeygen(sk)
+    accountKey := FlowAccountKeygen(sk)
     pkString := "0x9cd98d436d111aab0718ab008a466d636a22ac3679d335b77e33ef7c52d9c8ce47cf5ad71ba38cedd336402aa62d5986dc224311383383c09125ec0636c0b042"
-    assert.Equal(t, pkString, flowAccount.PublicKey.String())
+    assert.Equal(t, pkString, accountKey.PublicKey.String())
 }
 
 func TestClientConnection(t *testing.T) {
-    _, err := client.New("localhost:3569", grpc.WithInsecure())
+    seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
+    sk, _ := ECDSAKeygen(seed)
+
+    client, err := client.New("localhost:3569", grpc.WithInsecure())
     assert.NoError(t, err)
+
+    // Using this examples.ServiceAccount helper here
+    // https://github.com/onflow/flow-go-sdk/blob/master/examples/examples.go#L96
+    payer, payerKey, _ := examples.ServiceAccount(client)
+
+    accountKey := FlowAccountKeygen(sk)
+    script := templates.CreateAccount([]*flow.AccountKey{accountKey}, nil, payer)
+
+    tx := flow.NewTransaction().
+        SetScript(script).
+        SetGasLimit(100).
+        SetProposalKey(payer, payerKey.Index, payerKey.SequenceNumber).
+        SetPayer(payer)
 }
