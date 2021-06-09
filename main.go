d096bfc16fd5bc07e2b5f73e77fa2795b82dd4bc
Mark Henderson <henderson.mark@gmail.com>
2021-06-09 11:24:25 -0400

    ECDSAKeygen and FlowAccountKeygen implementations

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
+// import "log"
+
 // import "github.com/onflow/cadence"
 
-func Sum(x int, y int) int {
-    return x + y
+import (
+    "github.com/onflow/flow-go-sdk"
+    "github.com/onflow/flow-go-sdk/crypto"
+    // "github.com/onflow/flow-go-sdk/templates"
+)
+
+func ECDSAKeygen(seed []byte) (crypto.PrivateKey, error) {
+    return crypto.GeneratePrivateKey(crypto.ECDSA_P256, seed)
+}
+
+func FlowAccountKeygen(privateKey crypto.PrivateKey) *flow.AccountKey {
+    publicKey := privateKey.PublicKey()
+
+    accountKey := flow.NewAccountKey().
+        SetPublicKey(publicKey).
+        SetHashAlgo(crypto.SHA3_256).               // pair this key with the SHA3_256 hashing algorithm
+        SetWeight(flow.AccountKeyWeightThreshold)   // give this key full signing weight
+
+   return accountKey
 }
 
 func main() {
-    Sum(5, 5)
+    seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
+    pk, _ := ECDSAKeygen(seed) // TODO err handling
+
+    FlowAccountKeygen(pk)
 }
--- main_test.go
+++ main_test.go
@@ -18,10 +18,10 @@ func TestECDSAKeygen(t *testing.T) {
 
 func TestFlowAccountFromKeygen(t *testing.T) {
     seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
-    pk, err := ECDSAKeygen(seed)
+    sk, err := ECDSAKeygen(seed)
     assert.NoError(t, err)
 
-    flowAccount := FlowAccountKeygen(pk)
+    flowAccount := FlowAccountKeygen(sk)
     pkString := "0x9cd98d436d111aab0718ab008a466d636a22ac3679d335b77e33ef7c52d9c8ce47cf5ad71ba38cedd336402aa62d5986dc224311383383c09125ec0636c0b042"
     assert.Equal(t, pkString, flowAccount.PublicKey.String())
 }
