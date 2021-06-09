7ae156100a64299d10dab3b788788ce9ba46e1ab
Mark Henderson <henderson.mark@gmail.com>
2021-06-09 11:23:34 -0400

    ECDSA + Account Keygen tests and testify/assert module

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/onflow/cadence v0.17.0 // indirect
 	github.com/onflow/flow-go-sdk v0.20.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,10 +1,27 @@
 package main
 
-import "testing"
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+    "github.com/onflow/flow-go-sdk/crypto"
+)
 
-func TestSum(t *testing.T) {
-    total := Sum(5, 5)
-    if total != 10 {
-       t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
-    }
+func TestECDSAKeygen(t *testing.T) {
+    seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
+    sk, err := ECDSAKeygen(seed)
+    assert.NoError(t, err)
+
+    assert.Equal(t, crypto.ECDSA_P256, sk.Algorithm())
+    skString := "0x68ee617d9bf67a4677af80aaca5a090fcda80ff2f4dbc340e0e36201fa1f1d8c"
+    assert.Equal(t, skString, sk.String())
+}
+
+func TestFlowAccountFromKeygen(t *testing.T) {
+    seed := []byte("elephant ears space cowboy octopus rodeo potato cannon pineapple")
+    pk, err := ECDSAKeygen(seed)
+    assert.NoError(t, err)
+
+    flowAccount := FlowAccountKeygen(pk)
+    pkString := "0x9cd98d436d111aab0718ab008a466d636a22ac3679d335b77e33ef7c52d9c8ce47cf5ad71ba38cedd336402aa62d5986dc224311383383c09125ec0636c0b042"
+    assert.Equal(t, pkString, flowAccount.PublicKey.String())
 }
