5ed2e265617d713127ab93fbda46db7011a4b7c6
Mark Henderson <henderson.mark@gmail.com>
2021-06-12 13:24:26 -0400

    TransferToken working

--- main.go
+++ main.go
@@ -7,9 +7,37 @@ import (
 	"github.com/onflow/cadence"
 	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go-sdk/client"
+	"github.com/onflow/flow-go-sdk/crypto"
 )
 
-func AddVaultToAccount() {
+// TODO: Better sk handling here
+func AddVaultToAccount(
+	ctx context.Context,
+	flowClient *client.Client,
+	account *flow.Account,
+	skString string,
+) error {
+	txScript, err := ioutil.ReadFile("./transactions/setup_account.cdc")
+
+	key1 := account.Keys[0]
+
+	privateKey, err := crypto.DecodePrivateKeyHex(crypto.ECDSA_P256, skString)
+	key1Signer := crypto.NewInMemorySigner(privateKey, key1.HashAlgo)
+
+	referenceBlock, err := flowClient.GetLatestBlock(ctx, true)
+
+	tx := flow.NewTransaction().
+		SetScript(txScript).
+		SetGasLimit(100).
+		SetProposalKey(account.Address, key1.Index, key1.SequenceNumber).
+		SetPayer(account.Address).
+		SetReferenceBlockID(referenceBlock.ID).
+		AddAuthorizer(account.Address)
+
+	err = tx.SignEnvelope(account.Address, key1.Index, key1Signer)
+
+	err = flowClient.SendTransaction(ctx, *tx)
+	return err
 }
 
 func GetSupply(ctx context.Context, flowClient *client.Client) (cadence.UFix64, error) {
@@ -27,7 +55,67 @@ func GetBalance(ctx context.Context, flowClient *client.Client, address flow.Add
 	value, err := flowClient.ExecuteScriptAtLatestBlock(ctx, script, []cadence.Value{
 		cadence.Address(address),
 	})
+	if err != nil {
+		return 0, err
+	}
 
 	balance := value.(cadence.UFix64)
-	return balance, err
+	return balance, nil
+}
+
+func TransferTokens(
+	ctx context.Context,
+	flowClient *client.Client,
+	amount cadence.UFix64,
+	from *flow.Account,
+	toAddress flow.Address,
+	skString string,
+) error {
+	txScript, err := ioutil.ReadFile("./transactions/transfer_tokens.cdc")
+	if err != nil {
+		return err
+	}
+
+	privateKey, err := crypto.DecodePrivateKeyHex(crypto.ECDSA_P256, skString)
+	if err != nil {
+		return err
+	}
+
+	key1 := from.Keys[0]
+	key1Signer := crypto.NewInMemorySigner(privateKey, key1.HashAlgo)
+
+	referenceBlock, err := flowClient.GetLatestBlock(ctx, true)
+	if err != nil {
+		return err
+	}
+
+	tx := flow.NewTransaction().
+		SetScript(txScript).
+		SetGasLimit(100).
+		SetProposalKey(from.Address, key1.Index, key1.SequenceNumber).
+		SetPayer(from.Address).
+		SetReferenceBlockID(referenceBlock.ID).
+		AddAuthorizer(from.Address)
+
+	err = tx.AddArgument(cadence.UFix64(amount))
+	if err != nil {
+		return err
+	}
+
+	err = tx.AddArgument(cadence.Address(toAddress))
+	if err != nil {
+		return err
+	}
+
+	err = tx.SignEnvelope(from.Address, key1.Index, key1Signer)
+	if err != nil {
+		return err
+	}
+
+	err = flowClient.SendTransaction(ctx, *tx)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -16,10 +16,6 @@ func TestAccountsCreated(t *testing.T) {
 	flowClient, err := client.New(os.Getenv("RPC_ADDRESS"), grpc.WithInsecure())
 	assert.NoError(t, err)
 
-	// Using this examples.ServiceAccount helper here
-	// https://github.com/onflow/flow-go-sdk/blob/master/examples/examples.go#L96
-	//payer, payerKey, payerSigner := examples.ServiceAccount(flowClient)
-
 	events, err := flowClient.GetEventsForHeightRange(ctx, client.EventRangeQuery{
 		Type:        "flow.AccountCreated",
 		StartHeight: 0,
@@ -46,7 +42,6 @@ func TestGetBalance(t *testing.T) {
 	assert.NoError(t, err)
 
 	balance, err := GetBalance(ctx, flowClient, flow.HexToAddress("0x01cf0e2f2f715450"))
-
 	assert.Equal(t, balance.String(), "1000.00000000")
 }
 
@@ -55,37 +50,13 @@ func TestAddVaultToAccount(t *testing.T) {
 	flowClient, err := client.New("localhost:3569", grpc.WithInsecure())
 	assert.NoError(t, err)
 
-	txScript, err := ioutil.ReadFile("./transactions/setup_account.cdc")
-	assert.NoError(t, err)
-
+	skString := "58125e2c18823b7914c625500e76e3006aa2e936bc9b9169f77ab951e84edefd"
 	accountA, err := flowClient.GetAccount(ctx, flow.HexToAddress("0x179b6b1cb6755e31"))
-	assert.NoError(t, err)
-
-	key1 := accountA.Keys[0]
-
-	privateKey, err := crypto.DecodePrivateKeyHex(crypto.ECDSA_P256, "58125e2c18823b7914c625500e76e3006aa2e936bc9b9169f77ab951e84edefd")
-	assert.NoError(t, err)
-	key1Signer := crypto.NewInMemorySigner(privateKey, key1.HashAlgo)
-
-	referenceBlock, err := flowClient.GetLatestBlock(ctx, true)
-	assert.NoError(t, err)
-
-	tx := flow.NewTransaction().
-		SetScript(txScript).
-		SetGasLimit(100).
-		SetProposalKey(accountA.Address, key1.Index, key1.SequenceNumber).
-		SetPayer(accountA.Address).
-		SetReferenceBlockID(referenceBlock.ID).
-		AddAuthorizer(accountA.Address)
-
-	err = tx.SignEnvelope(accountA.Address, key1.Index, key1Signer)
-	assert.NoError(t, err)
-
-	err = flowClient.SendTransaction(ctx, *tx)
+	err = AddVaultToAccount(ctx, flowClient, accountA, skString)
 	assert.NoError(t, err)
 
 	balance, err := GetBalance(ctx, flowClient, accountA.Address)
-
+	assert.NoError(t, err)
 	assert.Equal(t, balance.String(), "0.00000000")
 }
 
@@ -94,70 +65,36 @@ func TestNonVaultedAccount(t *testing.T) {
 	flowClient, err := client.New("localhost:3569", grpc.WithInsecure())
 	assert.NoError(t, err)
 
-	accountB, err := flowClient.GetAccount(ctx, flow.HexToAddress("0xf3fcd2c1a78f5eee"))
-	assert.NoError(t, err)
-
-	script, err := ioutil.ReadFile("./contracts/scripts/get_balance.cdc")
-	assert.NoError(t, err)
-
-	_, err = flowClient.ExecuteScriptAtLatestBlock(ctx, script, []cadence.Value{
-		cadence.Address(accountB.Address),
-	})
+	_, err = GetBalance(ctx, flowClient, flow.HexToAddress("0xf3fcd2c1a78f5eee"))
 	assert.Error(t, err)
 }
 
-func TestTransferAndTransferBack(t *testing.T) {
+func TestTransferTokens(t *testing.T) {
 	ctx := context.Background()
 	flowClient, err := client.New("localhost:3569", grpc.WithInsecure())
 	assert.NoError(t, err)
 
+	skFT := "5eb8df48667ac74981f4faaf8b425a6403c8729e90319a4cbfd7942b10e4622a"
 	accountFT, err := flowClient.GetAccount(ctx, flow.HexToAddress("0x01cf0e2f2f715450"))
 	assert.NoError(t, err)
 
+	skA := "58125e2c18823b7914c625500e76e3006aa2e936bc9b9169f77ab951e84edefd"
 	accountA, err := flowClient.GetAccount(ctx, flow.HexToAddress("0x179b6b1cb6755e31"))
 	assert.NoError(t, err)
 
-	key1 := accountFT.Keys[0]
-
-	privateKey, err := crypto.DecodePrivateKeyHex(crypto.ECDSA_P256, "5eb8df48667ac74981f4faaf8b425a6403c8729e90319a4cbfd7942b10e4622a")
-	assert.NoError(t, err)
-	key1Signer := crypto.NewInMemorySigner(privateKey, key1.HashAlgo)
-
-	txScript, err := ioutil.ReadFile("./contracts/scripts/transfer_tokens.cdc")
-
-	referenceBlock, err := flowClient.GetLatestBlock(ctx, true)
+	// Transfer 1 token from FT minter to Account A
+	err = TransferTokens(ctx, flowClient, 100000000, accountFT, accountA.Address, skFT)
 	assert.NoError(t, err)
 
-	tx := flow.NewTransaction().
-		SetScript(txScript).
-		SetGasLimit(100).
-		SetProposalKey(accountFT.Address, key1.Index, key1.SequenceNumber).
-		SetPayer(accountFT.Address).
-		SetReferenceBlockID(referenceBlock.ID).
-		AddAuthorizer(accountFT.Address)
+	balanceA, err := GetBalance(ctx, flowClient, flow.HexToAddress("0x179b6b1cb6755e31"))
+	assert.NoError(t, err)
+	assert.Equal(t, balanceA.String(), "1.00000000")
 
-	err = tx.AddArgument(cadence.UFix64(10.0))
+	// Transfer the 1 token back from account A to FT minter
+	err = TransferTokens(ctx, flowClient, 100000000, accountA, accountFT.Address, skA)
 	assert.NoError(t, err)
 
-	err = tx.AddArgument(cadence.Address(accountA.Address))
+	balanceFT, err := GetBalance(ctx, flowClient, flow.HexToAddress("0x01cf0e2f2f715450"))
 	assert.NoError(t, err)
-
-	err = tx.SignEnvelope(accountFT.Address, key1.Index, key1Signer)
-	assert.NoError(t, err)
-
-	//  Transfer from Account A to Account B
-	//  flow transactions send ./transactions/transfer_tokens.cdc \
-	//    --arg UFix64:500.0 \
-	//    --arg Address:0x"$ACCOUNT_B" \
-	//    --signer=ft-account
-	//
-	//  flow scripts execute ./contracts/scripts/get_balance.cdc --arg Address:0x"$ACCOUNT_B"
-	//
-	//  Transfer from Account B back to Account A
-	//  flow transactions send ./transactions/transfer_tokens.cdc \
-	//    --arg UFix64:50.0 \
-	//    --arg Address:0x"$ACCOUNT_A" \
-	//    --signer=receiver-account
-	//
-	//  flow scripts execute ./contracts/scripts/get_balance.cdc --arg Address:0x"$ACCOUNT_A"
+	assert.Equal(t, balanceFT.String(), "1000.00000000")
 }
