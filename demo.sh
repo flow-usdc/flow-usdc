9f495c4b34ecca4526e4a16be6d857eebeff3fc6
Mark Henderson <henderson.mark@gmail.com>
2021-06-14 19:59:29 -0400

    env var

--- demo.sh
+++ demo.sh
@@ -8,20 +8,22 @@ if [ "${NETWORK}" == "emulator" ]; then
   flow emulator &
   EMULATOR_PID=$!
   sleep 1
+  SIGNER=emulator-account
+  flow accounts create --network="$NETWORK" --key="$TOKEN_ACCOUNT_PK" --signer="$SIGNER"
+else
+  SIGNER=token-account
 fi
 
-
-flow accounts create --key="$TOKEN_ACCOUNT_PK"
 
 NEW_VAULTED_ACCOUNT_SEED=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random)
 NEW_VAULTED_ACCOUNT_SK=$(flow keys generate --seed="$NEW_VAULTED_ACCOUNT_SEED" -o inline --filter=Private)
 NEW_VAULTED_ACCOUNT_PK=$(flow keys generate --seed="$NEW_VAULTED_ACCOUNT_SEED" -o inline --filter=Public)
-NEW_VAULTED_ACCOUNT_ADDRESS=$(flow accounts --network="$NETWORK" create --key="$NEW_VAULTED_ACCOUNT_PK" -o inline --filter=Address)
+NEW_VAULTED_ACCOUNT_ADDRESS=$(flow accounts create --network="$NETWORK" --key="$NEW_VAULTED_ACCOUNT_PK" --signer="$SIGNER" -o inline --filter=Address)
 
 NON_VAULTED_ACCOUNT_SEED=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random)
 NON_VAULTED_ACCOUNT_SK=$(flow keys generate --seed="$NON_VAULTED_ACCOUNT_SEED" -o inline --filter=Private)
 NON_VAULTED_ACCOUNT_PK=$(flow keys generate --seed="$NON_VAULTED_ACCOUNT_SEED" -o inline --filter=Public)
-NON_VAULTED_ACCOUNT_ADDRESS=$(flow accounts --network="$NETWORK" create --key="$NON_VAULTED_ACCOUNT_PK" -o inline --filter=Address)
+NON_VAULTED_ACCOUNT_ADDRESS=$(flow accounts create --network="$NETWORK" --key="$NON_VAULTED_ACCOUNT_PK" --signer="$SIGNER" -o inline --filter=Address)
 
 flow project deploy --network="$NETWORK" --update
 
