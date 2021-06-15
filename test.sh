354eebdfa4f19fa201956ef8e1161bec533fbcc9
Mark Henderson <henderson.mark@gmail.com>
2021-06-15 15:54:14 -0400

    funding newly generated testnet account for storage

--- test.sh
+++ test.sh
@@ -18,11 +18,18 @@ else
   SIGNER=token-account
 fi
 
-
 NEW_VAULTED_ACCOUNT_SEED=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random)
 NEW_VAULTED_ACCOUNT_SK=$(flow keys generate --seed="$NEW_VAULTED_ACCOUNT_SEED" -o inline --filter=Private)
 NEW_VAULTED_ACCOUNT_PK=$(flow keys generate --seed="$NEW_VAULTED_ACCOUNT_SEED" -o inline --filter=Public)
 NEW_VAULTED_ACCOUNT_ADDRESS=$(flow accounts create --network="$NETWORK" --key="$NEW_VAULTED_ACCOUNT_PK" --signer="$SIGNER" -o inline --filter=Address)
+
+if [ "${NETWORK}" == "testnet" ]; then
+  flow transactions send transactions/transfer_flow_tokens.cdc \
+    --arg=UFix64:0.001 \
+    --arg=Address:0x"$NEW_VAULTED_ACCOUNT_ADDRESS" \
+    --signer=token-account \
+    --network=testnet
+fi
 
 NON_VAULTED_ACCOUNT_SEED=$(hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random)
 NON_VAULTED_ACCOUNT_SK=$(flow keys generate --seed="$NON_VAULTED_ACCOUNT_SEED" -o inline --filter=Private)
